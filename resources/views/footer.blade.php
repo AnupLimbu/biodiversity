ce4ebfe52debbc5df9779ae1d825716f424b7fb9
kaushalbro <official.kaushalg@gmail.com>
2024-08-18 12:46:48 +0545

    Footer Completed

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,57 @@
+ <section id="footer" class=" bg-[#000000]" style="padding:50px 0 50px 0">
+        <div class="max-w-screen-xl mx-auto  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4" style="color: white">
+            <div style="width:100%; display: flex; flex-direction: column;padding: 10px;align-items: center">
+                <img src="  {{asset('images/biodiversity_logo.png')}}" alt="" style="margin-top: -59px;">
+                <p class="" style="text-align: center">
+                    Create a world where the diversity of life on Earth is preserved, protected, and valued
+                </p>
+            </div>
+            <div style="width:100%; display: flex; flex-direction: column; gap: 25px;padding: 10px ">
+                <h2 style="font-size: larger; font-weight: bold">Links</h2>
+                <ul style="display: flex; flex-direction: column; gap: 10px">
+                    <li style="display: flex; flex-direction: column; gap: 6px">
+                        <a href="/">Completed projects</a>
+                        <hr>
+                    </li>
+                    <li style="display: flex; flex-direction: column; gap: 6px">
+                        <a href="/">Ongoing projects</a>
+                        <hr>
+                    </li>
+                    <li style="display: flex; flex-direction: column; gap: 6px">
+                        <a href="/">Gallery</a>
+                        <hr>
+                    </li>
+                    <li style="display: flex; flex-direction: column; gap: 6px">
+                        <a href="/">Our Team</a>
+                        <hr>
+                    </li>
+                    <li style="display: flex; flex-direction: column; gap: 6px">
+                        <a href="/">Our Supporters</a>
+                        <hr>
+                    </li>
+                    <li style="display: flex; flex-direction: column; gap: 6px">
+                        <a href="/">Gallery</a>
+                        <hr>
+                    </li>
+                </ul>
+            </div>
+            <div style="width:100%; display: flex; flex-direction: column; gap: 25px ;padding: 10px">
+                <h2 style="font-size: larger; font-weight: bold">Contact Us</h2>
+                <ul style="display: flex; flex-direction: column; gap: 25px">
+                    <li style="display: flex;  gap: 10px">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <p  style="margin-top: -3px">Chandragiri Municipality-14, Kathmandu 44619, Nepal</p>
+                    </li>
+                    <li style="display: flex;  gap: 10px">
+                        <i class="fa-solid fa-phone-volume"></i>
+                        <p  style="margin-top: -3px"><a href="#">+977-98xxxxxxxx,&nbsp;&nbsp;+977-98xxxxxxxx</a></p>
+                    </li>
+                    <li style="display: flex;  gap: 10px">
+                        <i class="fa-regular fa-envelope-open"></i>
+                        <p  style="margin-top: -3px"><a href="mailto:info@conservationhimalaya.org" style="text-decoration: underline">info@conservationhimalaya.org</a></p>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </section>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,6 +3,26 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bio Diversity Research & Conservation Society</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+{{--    SEO DATA--}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <meta name="robots" content="index, follow" />
+    <meta name="COUNTRY" content="global" />
+    <meta name="LANGUAGE" content="en" />
+    <meta name="REGION" content="global" />
+    <meta property="og:url" content="" />
+    <meta property="og:locale" content="en" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="" />
+    <meta property="og:description" content="" />
+    <link rel="shortcut icon" href="" />
+    <link rel="canonical" href="" />
+{{--    SEO DATA--}}
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body >
@@ -10,6 +30,7 @@
     <main>
         @yield('content')
     </main>
+    @include('footer')
 </body>
 <script>
     document.addEventListener('DOMContentLoaded', () => {
