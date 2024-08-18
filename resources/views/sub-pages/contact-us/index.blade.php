8eda75ec31ee4d1dde9633d540c359afd6aea93d
kaushalbro <official.kaushalg@gmail.com>
2024-08-18 13:16:57 +0545

    Change placeholder name

--- resources/views/sub-pages/contact-us/index.blade.php
+++ resources/views/sub-pages/contact-us/index.blade.php
@@ -1,6 +1,6 @@
 @extends('master')
 @section('content')
-    <section class="container mx-auto  max-w-screen-xl" style="margin: 100px auto 60px auto">
+    <section class="container mx-auto  max-w-screen-xl" style="margin: 100px auto 60px auto; padding: 10px">
         <form>
             <div class="space-y-12">
                 <div class="border-b border-gray-900/10 pb-12">
@@ -10,19 +10,19 @@
                         <div class="sm:col-span-3">
                             <label for="first-name" class="block text-sm  font-bold leading-6 text-gray-900">First name:</label>
                             <div class="mt-2">
-                                <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                <input type="text" placeholder="First Name" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:pl-[10px] focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                             </div>
                         </div>
                         <div class="sm:col-span-3">
                             <label for="last-name" class="block text-sm font-bold  leading-6 text-gray-900">Last name:</label>
                             <div class="mt-2">
-                                <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                <input type="text" placeholder="Last Name"  name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:pl-[10px] focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                             </div>
                         </div>
                         <div class="sm:col-span-4">
                             <label for="email" class="block text-sm font-bold leading-6 text-gray-900">Email address:</label>
                             <div class="mt-2">
-                                <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                <input id="email" name="email" placeholder="Email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:pl-[10px] focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                             </div>
                         </div>
                         <div class="col-span-full">
