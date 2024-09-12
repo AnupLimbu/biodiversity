e71419f554b84605a49a2dfdaa129d5fac0e6884
kaushalbro <official.kaushalg@gmail.com>
2024-09-12 09:08:45 +0545

    Minor change

--- resources/views/sub-pages/support_us/index.blade.php
+++ resources/views/sub-pages/support_us/index.blade.php
@@ -7,7 +7,7 @@
             <div class="lg:w-1/2 md:w-full">
                 <div class="lg:col-span-2 rounded-lg sm:p-10 p-4 z-10 max-lg:-order-1 max-lg:mb-8"  >
                     <div class=" max-w-2xl shadow overflow-hidden sm:rounded-lg" style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;">
-                        <div class="px-4 py-5 sm:px-6  " style="background-color: #e9f8f8">
+                        <div class="px-4 py-3 sm:px-6  " style="background-color: #e9f8f8">
                             <h2 class="text-2xl  text-center font-bold mb-1 text-green-800 underline">Support Us</h2>
                             <h3 class="text-lg  leading-6 font-medium text-green-800 text-center ">
                                 Donations Information
@@ -15,7 +15,7 @@
                         </div>
                         <div class="border-t border-gray-200">
                             <dl>
-                                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <div class="bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                     <dt class="text-gray-700" style="font-weight: bolder">
                                         Bank Name:
                                     </dt>
@@ -23,7 +23,7 @@
                                         NMB Bank
                                     </dd>
                                 </div>
-                                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <div class="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                     <dt class="text-sm font-medium text-gray-700" style="font-weight: bolder">
                                         Branch:
                                     </dt>
@@ -31,7 +31,7 @@
                                         Putalisadak Branch
                                     </dd>
                                 </div>
-                                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <div class="bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                     <dt class="text-sm font-medium text-gray-700" style="font-weight: bolder">
                                         Account No:
                                     </dt>
@@ -39,7 +39,7 @@
                                         0040143027000018
                                     </dd>
                                 </div>
-                                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <div class="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                     <dt class="text-sm font-medium text-gray-700" style="font-weight: bolder">
                                         Account Holder Name:
                                     </dt>
@@ -47,7 +47,7 @@
                                         Biodiversity Research and Conservation Society
                                     </dd>
                                 </div>
-                                <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6" style="display: flex; justify-content: center">
+                                <div class="bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6" style="display: flex; justify-content: center">
                                     <img src="https://www.imgonline.com.ua/examples/qr-code.png" alt="" style="width: 250px;height: 250px">
                                 </div>
                             </dl>
