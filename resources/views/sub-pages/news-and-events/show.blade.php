f9caeee9bb0f3e394a15a8be81b595af566290c1
Anup Limbu <imroastsausage@gmail.com>
2024-09-12 19:10:44 +0545

    update: changed news and events web view

--- resources/views/sub-pages/news-and-events/show.blade.php
+++ resources/views/sub-pages/news-and-events/show.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="navbar-filler  bg-[#abcea0]">
     </div>
-    <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white  antialiased">
-        <h2 class="text-center mb-5 text-2xl font-bold text-gray-900 ">{{$newsAndEvent->title}}</h2>
+    <main class="pt-8 pb-8 lg:pt-16 bg-white  antialiased">
+        <h2 class="text-center mb-5 text-4xl font-bold text-gray-900 ">{{$newsAndEvent->title}}</h2>
         <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
             <article class="mx-auto w-full  format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
 {{--                <div style="display: flex;justify-content: center;margin-bottom: 10px">--}}
@@ -29,22 +29,25 @@
 @if(count($newsAndEvents)>0)
     <aside aria-label="Related articles" class="py-4 bg-gray-50 ">
         <div class="container mx-auto">
-            <h2 class="my-4 text-2xl font-bold text-gray-900 ">Other Projects</h2>
+            <h2 class="my-4 text-2xl font-bold text-gray-900 ">Other News & Events</h2>
             <div class="grid gap-12 sm:grid-cols-2 lg:grid-cols-4">
                 @foreach($newsAndEvents as $entity)
                     @php($link="/news-and-events/".$entity->id)
-                    <article class=" bg-white shadow-4 rounded-lg p-4">
-                        <a href="{{$link}}" class="max-h-[200px] ">
-                            <img src="{{asset('storage/'.$entity->thumbnail)}}" class="mb-5 rounded-lg inset-0 w-full max-h-[200px]  object-cover" alt="Image 1">
-                        </a>
-                        <h2 class="mb-2 text-xl font-bold leading-tight text-gray-900 ">
-                            <a href="{{$link}}">{{$entity->title}}</a>
+                    <div class="bg-white shadow-lg rounded-lg p-4 flex flex-col h-full">
+                        <div class="relative h-48 mb-4">
+                            <img src="{{asset('storage/'.$entity->thumbnail)}}" class="w-full h-full object-cover rounded-lg" alt="Image 1">
+                        </div>
+                        <h2 class="mb-2 text-lg sm:text-xl font-bold leading-tight text-gray-900">
+                            <a href="{{$link}}" class="hover:text-primary-600">{{$entity->title}}</a>
                         </h2>
-                        <p class="mb-4 text-gray-500 dark:text-gray-400">{{$entity->description}}</p>
-                        <a href="{{$link}}" class="inline-flex items-center font-medium  underline-offset-4 text-primary-600 dark:text-primary-500 hover:underline">
+                        <p class="mb-4 text-gray-500 dark:text-gray-400 text-ellipsis overflow-hidden whitespace-nowrap">
+                            {{$entity->description}}
+                        </p>
+                        <a href="{{$link}}" class="inline-flex items-center font-medium underline-offset-4 hover:underline mt-auto">
                             Read More
                         </a>
-                    </article>
+                    </div>
+
                 @endforeach
             </div>
         </div>
