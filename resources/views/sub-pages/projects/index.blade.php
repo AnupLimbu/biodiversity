c9dc90aa2262cd764668d06fa2837b314abbd0da
kaushalbro <official.kaushalg@gmail.com>
2024-09-17 09:26:59 +0545

    Npm run build

--- resources/views/sub-pages/projects/index.blade.php
+++ resources/views/sub-pages/projects/index.blade.php
@@ -27,8 +27,8 @@
                                 </a>
                             </div>
                             <div class="pt-6 flex-grow">
-                                <h1 class="text-2xl mb-2 line-clamp-2">{{ \Illuminate\Support\Str::limit($project->title,35)}}</h1>
-                                <p class="text-gray-700 mb-2 ">
+                                <h1 class="text-2xl mb-2 line-clamp-1">{{ \Illuminate\Support\Str::limit($project->title,35)}}</h1>
+                                <p class="text-gray-700 mb-2 line-clamp-2">
                                     {!! \Illuminate\Support\Str::limit(strip_tags($project->description))!!}
                                 </p>
                             </div>
